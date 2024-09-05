eda6183a6faa0f46f4884130bda6adc269f5af13
WeddyNkatha265 <weddynkatha265@gmail.com>
2024-09-05 18:39:58 +0300

    Updates many to many

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}  # Initialize the grades dictionary
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -15,9 +17,26 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
+    def add_grade(self, enrollment, grade):
+        if enrollment in self._enrollments:
+            self._grades[enrollment] = grade
+        else:
+            raise ValueError("The enrollment does not exist for this student")
+
+    def aggregate_average_grade(self):
+        if not self._grades:
+            return 0  # Handle case with no grades
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        average_grade = total_grades / num_courses
+        return average_grade
+
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -33,7 +52,7 @@ class Course:
 
 class Enrollment:
     all = []
-    
+
     def __init__(self, student, course):
         if isinstance(student, Student) and isinstance(course, Course):
             self.student = student
@@ -45,3 +64,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
